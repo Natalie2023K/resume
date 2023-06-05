9e5f318e1bb081bb154a4094197300c603510315
Natalie2023K <natakirilcuk@gmail.com>
2023-06-05 11:03:47 +0300

    task21.1

--- src/route/index.js
+++ src/route/index.js
@@ -1129,6 +1129,7 @@ router.get('/task21', function (req, res) {
 
   //            ↙ cюди вводимо назву файлу з сontainer
   res.render('task21', {
+    layout: 'task21',
     heading: {
       main: {
         title: 'JavaScript',
